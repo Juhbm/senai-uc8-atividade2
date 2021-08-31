566a13ab682ad6077786befa2f862a013a207d3a
juhbm <jubmz@hotmail.com>
2021-08-30 22:41:58 -0300

    Adição de comentários no cadastro de eventos.

--- cadastroDeEvento.js
+++ cadastroDeEvento.js
@@ -1,8 +1,10 @@
-let dataEvento = 20210905;
+// declaração das variáveis.
+
+let dataEvento = 20210905; // data inserida anomêsdia para ser processada adequadamente na condição if.
 let idade = 20;
 let listaParticipantes = ['Juliana', 'Vanessa', 'Nathaly', 'Danielle', 'Mayara', 'Joelma', 'Oberdan', 'Messias', 'Matheus', 'Maicon', 'Francisco', 'Leandro'];
 let listaPalestrantes = ['Odirlei', 'Thiago', 'Raoni']
-let total = listaParticipantes.length + listaPalestrantes.length
+let total = listaParticipantes.length + listaPalestrantes.length //soma total de participantes e palestrantes.
 
 if (dataEvento > 20210905){
     console.log("Inscrição não permitida! Data inválida. Tente novamente!")
@@ -16,8 +18,12 @@ if (idade >= 18) {
     console.log("Menor de 18 anos!!! Não permitido.")
 }
 
+// apresentação dos participantes e palestrantes no terminal e o total deles
+
 console.log(' ===== LISTA DE PARTICIPANTES ==== \n', listaParticipantes, ' ==== LISTA DE PALESTRANTES ==== \n', listaPalestrantes)
 console.log(' TOTAL: ', total)
+
+// junção das listas para validar a condição de quantidade de pessoas do evento
 
 if (listaParticipantes.length + listaPalestrantes.length < 100 ) {
     console.log("Cadastro permitido.")   
