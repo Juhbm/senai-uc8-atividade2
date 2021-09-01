54a568e5aa6085d4bff933704a0fa5ec0babc80b
juhbm <jubmz@hotmail.com>
2021-08-31 21:29:23 -0300

    Correção da estrutura condicional IF/ELSE e outros detalhes.

--- cadastroDeEvento.js
+++ cadastroDeEvento.js
@@ -1,32 +1,51 @@
 // declaração das variáveis.
 
-let dataEvento = 20210905; // data inserida anomêsdia para ser processada adequadamente na condição if.
-let idade = 20;
+let dataInsc = 2021.0905
+let dataEvento = 2021.0905 // data inserida anomêsdia(AAAAMMDD) para ser processada adequadamente na condição if sem precisar de recurso new date.
+let dataNasc = 2002.0905 // (O '.' (AAAA.MMDD) após o ano é para emitir a idade com 2 dígitos.)
+let idadeMin18 = 18.0000;
+let idade = dataEvento - dataNasc;
 let listaParticipantes = ['Juliana', 'Vanessa', 'Nathaly', 'Danielle', 'Mayara', 'Joelma', 'Oberdan', 'Messias', 'Matheus', 'Maicon', 'Francisco', 'Leandro'];
 let listaPalestrantes = ['Odirlei', 'Thiago', 'Raoni']
 let total = listaParticipantes.length + listaPalestrantes.length //soma total de participantes e palestrantes.
 
-if (dataEvento > 20210905){
-    console.log("Inscrição não permitida! Data inválida. Tente novamente!")
+console.log("\n |======= CADASTRO DE EVENTO =======| \n")
+console.log(" Esse cadastro é bem rapidinho, precisamos de pequenas informações.\n")
+console.log(" Por favor, digite a data que deseja participar do Evento (AAAA.MMDD): ", dataInsc)
+
+// estrutura condicional para permitir cadastro no evento:
+
+if (dataInsc <= dataEvento){
+    console.log('\n Inscrição permitida! Vamos prosseguir...')
+    console.log('\n... agora digite sua Data de Nascimento (AAAA.MMDD): ', dataNasc)
+
+    if (dataNasc <= dataEvento){
+        console.log('\n Sua idade é: ', idade.toFixed(0), ' anos.') // uso do tofixed para fixar casas decimais, no caso = 0.
+    }
+        if (idade >= idadeMin18){
+            console.log("\n Perfeito, você é Maior de 18 anos! Verificando a capacidade de pessoas permitida neste Evento...")
+
+            // apresentação dos participantes e palestrantes no terminal e o total deles
+
+            console.log('\n ===== LISTA DE PARTICIPANTES ==== \n\n', listaParticipantes, '\n ==== LISTA DE PALESTRANTES ==== \n\n', listaPalestrantes)
+            console.log('\n LIMITE MÁX. PERMITIDO: 100 PESSOAS.\n')
+            console.log(' TOTAL DE INSCRITOS ATÉ O MOMENTO: ', total, '\n')
+
+            // junção das listas para validar a condição de quantidade de pessoas do evento
+
+            if (listaParticipantes.length + listaPalestrantes.length < 100 ) {
+                console.log(" Parabéns! Você poderá participar do Evento. Estamos finalizando sua inscrição... \n")
+            } 
+                // condições else que impedem o cadastro:
+
+                else {
+                console.log(" Desculpe, capacidade excedida! Agradecemos seu interesse e aguardamos sua inscrição para o próximo Evento.\n")
+            }
+
+        } else{
+            console.log("\n Ahh... você é Menor de 18 anos! Cadastro não permitido. Não fique triste! Ao fazer 18 anos, poderá participar.\n")
+        }
+        
 } else {
-    console.log('Inscrição permitida.')
-}
-
-if (idade >= 18) {
-    console.log("Maior de 18 anos. Permitir cadastro.")
-} else {
-    console.log("Menor de 18 anos!!! Não permitido.")
-}
-
-// apresentação dos participantes e palestrantes no terminal e o total deles
-
-console.log(' ===== LISTA DE PARTICIPANTES ==== \n', listaParticipantes, ' ==== LISTA DE PALESTRANTES ==== \n', listaPalestrantes)
-console.log(' TOTAL: ', total)
-
-// junção das listas para validar a condição de quantidade de pessoas do evento
-
-if (listaParticipantes.length + listaPalestrantes.length < 100 ) {
-    console.log("Cadastro permitido.")   
-} else {
-    console.log("Capacidade excedida!")
+    console.log("\n Inscrição não permitida! Data inválida. Tente novamente! A data limite para sua inscrição é: \n", dataEvento)
 }
